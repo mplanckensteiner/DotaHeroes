04afd425ecabe534e0c2ad9d15eeb9a059a6727e
Miguel Planckensteiner <mike.planck@hotmail.com>
2021-02-06 18:06:34 +0100

    Initial Commit

---
+++ DotaHeroes/DotaHeroesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DotaHeroesApp.swift
+//  DotaHeroes
+//
+//  Created by Miguel Planckensteiner on 2/6/21.
+//
+
+import SwiftUI
+
+@main
+struct DotaHeroesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
